b4c6a70278457d1024d45a68cd5add4ea92a8bfb
Vivian Scouten <VivianScouten@proton.me>
2024-01-05 10:26:41 -0800

    Remove unused code in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,18 +4,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs }: {
 
     nixosConfigurations = {
       Glados = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
         modules = [
           ./configuration.nix
         ];
-        networking.hostName = "glados";
       };
 
     };
