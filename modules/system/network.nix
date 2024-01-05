65f93448ff37e8ad3efea8df28460244a46ff3ef
Vivian Scouten <VivianScouten@proton.me>
2024-01-04 16:25:03 -0800

    Add network configuration and firewall rules

---
+++ modules/system/network.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ...}:
+
+{
+  networkmanager.enable = true;
+  networking.nameservers = [ "8.8.8.8" ];
+
+  # Allow ALVR ports in firewall
+  networking.firewall.allowedTCPPorts = [ 8082 9943 9944 ];
+  networking.firewall.allowedUDPPorts = [ 8082 9943 9944 ];   
+}
