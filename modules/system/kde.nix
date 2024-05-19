394918af9b6bf6b4be222f0a008d6b11ae7c1445
Vivian Scouten <VivianScouten@proton.me>
2024-05-19 15:23:58 -0700

    Set amdgpu video driver in KDE configuration

--- modules/system/kde.nix
+++ modules/system/kde.nix
@@ -4,6 +4,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   environment.systemPackages = with pkgs; [
     libsForQt5.yakuake
